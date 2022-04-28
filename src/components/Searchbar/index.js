156b12d6023f4e119ea7255147ddb87d5d1522a9
LinneaEriksson <Linnea-E@hotmail.com>
2022-04-28 13:05:51 +0200

    testing

--- src/components/Searchbar/index.js
+++ src/components/Searchbar/index.js
@@ -1,6 +1,14 @@
 import "./style.css";
 
 const SearchBar = () => (
+
+  constructor(){
+    super();
+    this.state={name: ''}
+  }
+
+  render() {
+    return (
   <div className="searchBarContainer">
     <form action="/" method="get">
       <label htmlFor="searchField"></label>
@@ -10,12 +18,17 @@ const SearchBar = () => (
         id="header-search"
         placeholder="Search for a zodiac"
         name="searchField"
+        value={this.state.name}
+        onChange={(e)=>{this.setState({name: e.target.value})}}
       />
       <button className="searchButton" type="submit">
         Search
       </button>
+      <p>{this.state.name}</p>
     </form>
   </div>
+    )
+  }
 );
 
 export default SearchBar;
