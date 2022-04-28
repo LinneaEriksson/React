787fb3f529bee405923c43f42789c3d0f4834064
LinneaEriksson <Linnea-E@hotmail.com>
2022-04-28 17:03:34 +0200

    moved code

--- src/components/Aztro/index.js
+++ src/components/Aztro/index.js
@@ -21,45 +21,17 @@ class Aztro extends Component {
       });
   }
 
-  zodiacArrays() {
-    const zodiacs = [
-      "aries",
-      "taurus",
-      "gemini",
-      "cancer",
-      "leo",
-      "virgo",
-      "libra",
-      "scorpio",
-      "sagittarius",
-      "capricorn",
-      "aquarius",
-      "pisces",
-    ];
-
-    return zodiacs.map((zodiac) => (
-      <div className="zodiacContainer">
-        <img src={`/images/${zodiac}.jpg`} />
-        {zodiac}
-        <Button text="Read more" />
-      </div>
-    ));
-  }
-
   render() {
     return (
-      <div className="zodiacWrapper">
-        {this.zodiacArrays()}
-        <div className="astroBox">
-          Current Date: {this.state.json.current_date} <br />
-          Compatibility: {this.state.json.compatibility} <br />
-          Lucky Number: {this.state.json.lucky_number} <br />
-          Lucky Time: {this.state.json.lucky_time} <br />
-          Color: {this.state.json.color} <br />
-          Date Range: {this.state.json.date_range} <br />
-          Mood: {this.state.json.mood} <br />
-          Description: {this.state.json.description} <br />
-        </div>
+      <div className="astroBox">
+        Current Date: {this.state.json.current_date} <br />
+        Compatibility: {this.state.json.compatibility} <br />
+        Lucky Number: {this.state.json.lucky_number} <br />
+        Lucky Time: {this.state.json.lucky_time} <br />
+        Color: {this.state.json.color} <br />
+        Date Range: {this.state.json.date_range} <br />
+        Mood: {this.state.json.mood} <br />
+        Description: {this.state.json.description} <br />
       </div>
     );
   }
