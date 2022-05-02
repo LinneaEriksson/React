6866707f103e7964d43163ab4ebe8d941f05895e
JennAnd <89971271+JennAnd@users.noreply.github.com>
2022-05-02 15:06:56 +0200

    routing
    start routing between pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./App.css";
 import React from "react";
 import Aztro from "./components/Aztro";
 import Zodiacs from "./components/Zodiacs";
+import { Routes, Route } from "react-router-dom";
+import ZodiacInfo from "./pages/ZodiacInfo";
 
 // import SearchBar from "./components/Searchbar";
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/zodiac" element={<ZodiacInfo />} />
+        <Route path="/" element={<App />} />
+      </Routes>
       <section className="startPageSection">
         {/* <SearchBar /> */}
         <Zodiacs />
